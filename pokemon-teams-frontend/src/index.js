deaf5da0d0b5b9ddc5bfce42ceed80902b648a3e
alanbanks229 <alanbanks229@gmail.com>
2020-03-18 11:26:27 -0400

    Got trainer json stuff working

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,20 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    fetchTrainers() //Makes cards
+
+})
+
+
+function fetchTrainers(){
+    fetch(TRAINERS_URL)
+        .then(resp => resp.json())
+        .then(json => json.forEach( trainer => makeCard(trainer)))
+}
+
+function makeCard(trainer){
+    console.log(trainer)
+}
